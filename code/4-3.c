685f2a082be6627987b93c573ab4d685756c4225
eunbean-ops <rida1223@naver.com>
2026-04-04 16:19:06 +0900

    4-3

---
+++ code/4-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5]= {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
+
+    scanf("%d", &index);
+    
+    if( index>4){
+        printf("%s","Error!");
+    }else{
+        printf("%d", arr[index]);
+    }
+}
